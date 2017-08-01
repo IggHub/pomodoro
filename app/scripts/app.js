22867875b194db4ec83cf618591a3b0cc8ad5abe
Iggy <igoririanto@rocketmail.com>
2017-08-01 09:45:32 -0700

    clear up unused code

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,13 +1,6 @@
 var app = angular.module('myPomodoro', ['firebase']);
 
 app.controller('pomodoroCtrl', function($scope, $interval, $firebaseObject, $firebaseArray) {
-    console.log("pomodoro controller loaded");
-    //var firebaseRootRef = firebase.database().ref(); //points to root
-    //console.log(firebaseRootRef);
-    //$scope.roottasks = $firebaseArray(firebaseRootRef);
-    //console.log($scope.roottasks);
-    //$scope.roottasks.$remove();
-
     /* scopes; rename for clarity */
     $scope.countDownPomodoro = 1500; // 1500s = 25 min
     $scope.countDownBreak = 300;
@@ -116,44 +109,6 @@ app.controller('pomodoroCtrl', function($scope, $interval, $firebaseObject, $fir
     $scope.resetLongBreak = function(){
       $scope.countDownLongBreak = 1800;
       };
-
-/*    var ref = firebase.database().ref().child("data");
-    // download the data into a local object
-    var syncObject = $firebaseObject(ref);
-    // synchronize the object with a three-way data binding
-    // click on `index.html` above to see it used in the DOM!
-    syncObject.$bindTo($scope, "data");
-*/
-/*
-    $scope.newList = []; //creates empty array
-    $scope.addItemToList = function(itemlist) { //new method addItemToList
-      console.log(itemlist); //checks itemList
-      $scope.newList.push({ // why .newList.push? What does push do? It adds the new object into the end of array newList (currently [])
-        "item": itemlist,
-        "done": false
-      });
-    };
-
-    $scope.sendToDb = function() {
-      var ref = new Firebase("https://xxxxxx.firebaseio.com");
-      var list = $firebaseArray(ref);
-      list.$add({
-        "list": $scope.newList
-      }).then(function(ref) {
-        var id = ref.key();
-        console.log("added record with id " + id);
-        console.log(list.$indexFor(id)); // returns location in the array
-      })
-
-*/
-
-    /* firebase array */
-/*    var ref = firebase.database().ref();//.child("data");
-    var list = $firebaseArray(ref);
-    $scope.addArray = function() {
-      list.$add({foo: "bar"})
-    }
-*/
 
     var ref = firebase.database().ref().child("tasks"); //points to child ('tasks') from root
 
